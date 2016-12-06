a21ab7df5cd0aa8678983f5777d256a003c689ce
Karl Penzhorn <karlpenzhorn@gmail.com>
2016-12-06 19:30:55 +0200

    Correct Star Trek ages

--- src/main/java/tutorial/DatabaseLoader.java
+++ src/main/java/tutorial/DatabaseLoader.java
@@ -16,9 +16,9 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        this.repository.save(new Employee("Jean-Luc Picard", 45, 5));
-        this.repository.save(new Employee("William Riker", 54, 8));
-        this.repository.save(new Employee("Geordi La Forge", 34, 12));
-        this.repository.save(new Employee("Beverly Crusher", 33, 2));
+        this.repository.save(new Employee("Jean-Luc Picard", 59, 5));
+        this.repository.save(new Employee("William Riker", 31, 8));
+        this.repository.save(new Employee("Geordi La Forge", 30, 12));
+        this.repository.save(new Employee("Beverly Crusher", 40, 2));
     }
 }
